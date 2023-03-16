1e1d494cea9d435990360ab7fcddeb17fa71a782
Havvazeynep <havva.zeynep.16@gmail.com>
2023-03-16 22:41:52 +0300

    Json verilerini düzenleme ve filtreleme işlemleri yapıldı.

--- Ex-2/project-planner/src/views/Home.vue
+++ Ex-2/project-planner/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="current = $event" :current="current"></FilterNav>
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.şd">
+      <div v-for="project in filteredProjects" :key="project.şd">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
       </div>
     </div>
@@ -10,15 +11,17 @@
 
 <script>
 import SingleProject from '../components/SingleProject.vue'
-
+import FilterNav from '../components/FilterNav.vue'
 export default {
   name: 'Home',
   components: {
-    SingleProject
+    SingleProject,
+    FilterNav
   },
   data() {
     return {
-      projects: []
+      projects: [],
+      current: 'all',
     }
   },
   mounted() {
@@ -36,5 +39,16 @@ export default {
       p.complete = !p.complete
     }
   },
+  computed: {
+    filteredProjects() {
+      if(this.current === 'completed') {
+        return this.projects.filter(project => project.complete)
+      }
+      if(this.current === 'ongoing') {
+        return this.projects.filter(project => !project.complete)
+      }
+      return this.projects
+    }
+  }
 }
 </script>
